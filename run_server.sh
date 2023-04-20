627419c4f88aba0d619d8ffa2cb0e1cf446bb8b0
Arnold <arnold.gauthier@gmail.com>
2023-04-20 15:27:42 +0200

    Chris fix for mutated binaries

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,6 @@ cp /penv-fix/angr/stack_pointer_tracker.py  /usr/local/lib/python3.8/dist-packag
 cp /penv-fix/angr/threading.py              /usr/local/lib/python3.8/dist-packages/angr/exploration_techniques/threading.py
 cp /penv-fix/angr/heap_base.py              /usr/local/lib/python3.8/dist-packages/angr/state_plugins/heap/heap_base.py
 cp /penv-fix/angr/__init__.py               /usr/local/lib/python3.8/dist-packages/angr/storage/memory_mixins/__init__.py
+cp /penv-fix/elf/elf.py                     /usr/local/lib/python3.8/dist-packages/cle/backends/elf/elf.py
 
 python3 SemaWebApp/SemaServer.py
